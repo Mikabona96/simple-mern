c634b79a21031ff5ca2a525e3428d2119f1a272c
Mika Bona <dmitriygolubapk@gmail.com>
2022-06-02 11:02:55 +0300

    2nd done

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -7,7 +7,7 @@ const User = require("../model/userModel")
 // @access Private
 const getGoals = asyncHandler (async (req, res) => {
 
-    const goals = await Goal.find()
+    const goals = await Goal.find({user: req.user.id})
 
     res.status(200).json(goals)
 })
@@ -22,7 +22,8 @@ const setGoal = asyncHandler (async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
 
     res.status(200).json(goal)
@@ -30,12 +31,25 @@ const setGoal = asyncHandler (async (req, res) => {
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
+
 const updateGoal = asyncHandler (async (req, res) => {
     const goal = await Goal.findById(req.params.id)
 
     if (!goal) {
         res.status(404)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
--- controllers/userController.js
+++ controllers/userController.js
@@ -27,21 +27,50 @@ const registerUser = asyncHandler (async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user.id)
         })
     } else {
         res.status(400)
         throw new Error('Invalid user data')
     }
-
-    res.json({message: 'Register user'})
 })
 
 const loginUser = asyncHandler (async (req, res) => {
+    const {email, password} = req.body
+
+    //check for user e-mail
+    const user = await User.findOne({email})
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user.id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+
     res.json({message: 'Login user'})
 })
+ 
 const getMe = asyncHandler (async (req, res) => {
-    res.json({message: 'User data display'})
+    const {_id, name, email} = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
 })
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
 
 module.exports = {registerUser, loginUser, getMe}
